a6b972dc2a729b4a8a28438e7dc19f13ce7730ec
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-09 14:19:24 +0100

    factory method for carsObjectArray

--- autos.js
+++ autos.js
@@ -13,12 +13,8 @@ YEAR: ${this.year}`;
 }
 
 class Cars {
-	constructor(cars = null) {
-		if (cars !== null) {
-			this.cars = cars;
-		} else {
-			this.cars = [];
-		}
+	constructor() {
+		this.cars = [];
 	}
 
 	addCar(car) {
@@ -28,16 +24,50 @@ class Cars {
 	listCars() {
 		return this.cars.map(car => car.display()).join('\r\n\r\n');
 	}
+
+	static instantiateCarsWithJavaScriptObjectArray(javaScriptObjectArray) {
+		const cars = new Cars();
+		// CHALLENGE: complete :-)
+	}
+
+	static instantiateCarsWithCarsObjectArray(carsObjectArray) {
+		const cars = new Cars();
+		carsObjectArray.forEach(car => cars.addCar(car));
+		return cars;
+	}
 }
 
 const car1 = new Car('Mercedes', 'yellow', 2019);
 const car2 = new Car('BMW', 'blue', 2017);
 const car3 = new Car('BMW', 'grey', 2018);
+// const cars = new Cars([car1, car2]);
+const cars = Cars.instantiateCarsWithCarsObjectArray([car1, car2]);
+console.log(cars.listCars());
 // console.log(car1.display());
 // console.log(car2.display());
 
-const cars = new Cars([car1, car2]);
+const carsFromApi = [
+	{
+		make: 'Audi',
+		color: 'green',
+		year: 2008
+	},
+	{
+		make: 'Audi',
+		color: 'red',
+		year: 2008
+	},
+	{
+		make: 'Audi',
+		color: 'blue',
+		year: 2008
+	}
+];
+// const cars = new Cars(carsFromApi);
+
 // cars.addCar(car1);
 // cars.addCar(car2);
-cars.addCar(car3);
-console.log(cars.listCars());
+// cars.addCar(car3);
+// console.log(cars.listCars());
+
+
