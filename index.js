1c45b76d2dd5327b349852219673e1a142d16895
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-09 11:47:23 +0100

    Books class finished

--- index.js
+++ index.js
@@ -21,12 +21,32 @@ NUMBER OF PAGES: ${this.numberOfPages}`;
 	}
 }
 
+class Books {
+	constructor() {
+		this.books = [];
+	}
+
+	addBook(book) {
+		this.books.push(book);
+	}
+
+	getBooksAsArray() {
+		return this.books.map(book => book.displayAsLine());
+	}
+
+	listBooks() {
+		return this.getBooksAsArray().join('\r\n');
+	}
+}
+
 // code smell
 const book = Book.instantiateBook('Sapiens', 'Yuval Harari', 543);
 const book2 = new Book('JavaScript Functional Programmign', 'Federico Kereki', 433);
-
 // console.log(book.displayAsLine());
 // console.log(book2.displayAsBlock());
+// console.log(book.displayAsBlock());
 
-console.log(book.displayAsBlock());
-
+const books = new Books();
+books.addBook(book);
+books.addBook(book2);
+console.log(books.listBooks());
