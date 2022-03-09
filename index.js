72a32f0e7cf5ff734a67abd9a32480cf9e168aed
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-09 11:27:31 +0100

    two display methods

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('test');
+class Book {
+	constructor(title, author, numberOfPages) {
+		this.title = title;
+		this.author = author;
+		this.numberOfPages = numberOfPages;
+	}
+
+	displayAsLine() {
+		return `TITLE: ${this.title}, AUTHOR: ${this.author}, NUMBER OF PAGES: ${this.numberOfPages}`;
+	}
+
+	displayAsBlock() {
+		return `TITLE: ${this.title}
+AUTHOR: ${this.author}
+NUMBER OF PAGES: ${this.numberOfPages}`;
+	}
+}
+
+const book = new Book('Sapiens', 'Yuval Harari', 543);
+
+// console.log(book.displayAsLine());
+console.log(book.displayAsBlock());
+
