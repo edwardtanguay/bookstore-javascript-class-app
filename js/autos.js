04eec82596f79ce0ee2b67ec477b11c8a89b94e3
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-03-09 15:15:30 +0100

    show html

--- js/autos.js
+++ js/autos.js
@@ -1,7 +1,11 @@
 import { Car } from './car.js';
 import { Cars } from './cars.js';
 
+const contentElem = document.querySelector('.content');
+
 const car1 = new Car({ color: 'yellow', year: 2019, make: 'Mercedes' });
 const car2 = new Car({ make: 'BMW', color: 'blue', year: 2017 });
 const car3 = new Car({ make: 'BMW', color: 'red', year: 2018 });
 const cars = Cars.instantiateCarsWithCarsObjectArray([car1, car2, car3]);
+
+contentElem.innerHTML = cars.exportAsHtml();
